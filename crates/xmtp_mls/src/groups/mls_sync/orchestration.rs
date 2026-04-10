86c723b7cc1fd619397730e8ec47174f8d96051f
Matt Galligan <matt.galligan@gmail.com>
2026-04-10 15:59:52 -0400

    refactor(mls_sync): extract sync orchestration

---
+++ crates/xmtp_mls/src/groups/mls_sync/orchestration.rs
@@ -0,0 +1,98 @@
+use super::*;
+
+impl<Context> MlsGroup<Context>
+where
+    Context: XmtpSharedContext,
+{
+    #[tracing::instrument]
+    pub async fn sync(&self) -> Result<SyncSummary, GroupError> {
+        let conn = self.context.db();
+
+        let epoch = self.epoch().await?;
+        tracing::info!(
+            inbox_id = self.context.inbox_id(),
+            installation_id = %self.context.installation_id(),
+            group_id = self.group_id.short_hex(),
+            "[{}] syncing group, epoch = {epoch}",
+            self.context.inbox_id(),
+        );
+
+        // Also sync the "stitched DMs", if any...
+        for other_dm in conn.other_dms(&self.group_id)? {
+            let other_dm = Self::new_from_arc(
+                self.context.clone(),
+                other_dm.id,
+                other_dm.dm_id.clone(),
+                other_dm.conversation_type,
+                other_dm.created_at_ns,
+            );
+
+            other_dm.sync_with_conn().await?;
+            other_dm.maybe_update_installations(None).await?;
+        }
+
+        let sync_summary = self.sync_with_conn().await.map_err(GroupError::from)?;
+        self.maybe_update_installations(None).await?;
+        Ok(sync_summary)
+    }
+
+    /// Sync from the network with the 'conn' (local database).
+    /// must return a summary of all messages synced, whether they were
+    /// successful or not.
+    #[cfg_attr(any(test, feature = "test-utils"), tracing::instrument(fields(who = %self.context.inbox_id())))]
+    #[cfg_attr(not(any(test, feature = "test-utils")), tracing::instrument(skip_all))]
+    pub async fn sync_with_conn(&self) -> Result<SyncSummary, SyncSummary> {
+        let _mutex = self.mutex.lock().await;
+        let mut summary = SyncSummary::default();
+
+        if !self.is_active().map_err(SyncSummary::other)? {
+            log_event!(
+                Event::GroupSyncGroupInactive,
+                self.context.installation_id(),
+                group_id = self.group_id
+            );
+            return Err(SyncSummary::other(GroupError::GroupInactive));
+        }
+
+        if let Err(e) = self.handle_group_paused() {
+            if matches!(e, GroupError::GroupPausedUntilUpdate(_)) {
+                // nothing synced
+                return Ok(summary);
+            } else {
+                return Err(SyncSummary::other(e));
+            }
+        }
+
+        // Even if publish fails, continue to receiving
+        let result = self.publish_intents().await;
+        if let Err(e) = result {
+            tracing::error!("Sync: error publishing intents {e:?}",);
+            summary.add_publish_err(e);
+        }
+
+        // Even if receiving fails, we continue to post_commit
+        // Errors are collected in the summary.
+        let result = self.receive().await;
+        match result {
+            Ok(s) => summary.add_process(s),
+            Err(e) => {
+                summary.add_other(e);
+                // We don't return an error if receive fails, because it's possible this is caused
+                // by malicious data sent over the network, or messages from before the user was
+                // added to the group
+            }
+        }
+
+        let result = self.post_commit().await;
+        if let Err(e) = result {
+            tracing::error!("post commit error {e:?}",);
+            summary.add_post_commit_err(e);
+        }
+
+        if summary.is_errored() {
+            Err(summary)
+        } else {
+            Ok(summary)
+        }
+    }
+}
